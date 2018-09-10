c0a32f2d7095e05a905d3f68bfb6e9b4fea5f76e
elliottdehnbostel <dehnbostele@gmail.com>
2018-09-10 15:55:04 -0400

    Fixed fragment related bug. Still some issues with code styling and name convention, but I am currently just working through the MVP of the app.

--- app/src/main/java/com/gregory/revelation/MainActivity.java
+++ app/src/main/java/com/gregory/revelation/MainActivity.java
@@ -4,6 +4,7 @@ import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,15 +13,21 @@ public class MainActivity extends AppCompatActivity implements OnFragmentInterac
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
+        ActionBar bar = getSupportActionBar();
+        if(bar != null){
+            bar.hide();
+        }
+
         setContentView(R.layout.activity_main);
 
-        Fragment fragment = new Intro();
+        if(savedInstanceState == null) { //this is done so that rotations don't reset the fragment
+            Fragment fragment = new Intro();
 
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction transaction = fm.beginTransaction();
-        transaction.replace(R.id.contentFragment, fragment);
-        transaction.commit();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction transaction = fm.beginTransaction();
+            transaction.replace(R.id.contentFragment, fragment);
+            transaction.commit();
+        }
     }
 
     public void onFragmentInteraction(int id) {
